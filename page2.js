8a3280e247dbb3a7fd082ee8909f0103f9767253
alanoudalsawagh <103546334+alanoudalsawagh@users.noreply.github.com>
2022-07-10 11:40:43 +0300

    done

---
+++ page2.js
@@ -0,0 +1,9 @@
+let subscribe= prompt(" هل تريد الاشتراك بمنصتي؟ رد بنعم او لا ");  
+let x="نعم"; 
+let y="لا"; 
+if (subscribe== x){
+    console.log("مرحبا بك في منصتي "); 
+} 
+else if(subscribe==y){
+    console.log("حسنا عد لنا في وقت لاحق"); 
+} 
